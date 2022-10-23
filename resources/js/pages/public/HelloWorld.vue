feb0bf82e8afbb1d3742c33cf76d3bf688bb376c
abdalodj <lodianea98@gmail.com>
2022-10-23 21:16:09 +0000

    Working on landing page layout

--- resources/js/pages/public/HelloWorld.vue
+++ resources/js/pages/public/HelloWorld.vue
@@ -1,53 +1,18 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
+  <v-container fluid style="height: 100vh;">
+    <v-row justify="center" class="my-4 h-100">
       <v-col cols="12">
-        <v-img
-          src="https://img2.freepng.fr/20180407/qcq/kisspng-vue-js-javascript-framework-front-and-back-ends-gi-technical-5ac9109e8adb43.6860172915231264305688.jpg"
-          class="my-3"
-          contain
-          height="200"
-        />
+        <div>
+          <v-img src="img/reading_list_pana.png"></v-img>
+        </div>
       </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a v-for="(next, i) in whatsNext" :key="i" :href="next.href" class="subheading mx-3" target="_blank">
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a v-for="(link, i) in importantLinks" :key="i" :href="link.href" class="subheading mx-3" target="_blank">
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a v-for="(eco, i) in ecosystem" :key="i" :href="eco.href" class="subheading mx-3" target="_blank">
-            {{ eco.text }}
-          </a>
-        </v-row>
+      <v-col cols="12" class="font-weight-bold" style="font-family: 'Caveat', cursive;">
+        <p class="text-center" style="font-size: 32px;">Bienvenu sur Nate Bi.</p>
+        <p class="text-center" style="font-size: 18px;">Plateforme de transparence et gestion de cotisation pour
+          l'amélioration de
+          la vie social
+          et
+          communautaire </p>
       </v-col>
     </v-row>
   </v-container>
@@ -60,64 +25,10 @@ export default Vue.extend({
   name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
   }),
-  mounted ()  {
-    this.showSwal();
+  mounted() {
   },
   methods: {
-    showSwal() {
-      this.$swal('worked', `${this.$shortDateFormat('01/02/1998')}`, 'success');
-    }
   }
 });
 </script>
--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,10 +11,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
 
-    <link rel="icon" type="image/png" sizes="16x16" href="https://entpersonnel.ucad.sn/ressources/img/logo_ucad.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="https://entpersonnel.ucad.sn/ressources/img/logo_ucad.png">
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
@@ -28,4 +33,5 @@
         <div id="back-office"></div>
     @endauth
 </body>
+
 </html>
