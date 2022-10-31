ab035442e02d5d46d81f514d20127ff7efe2e78b
abdalodj <lodianea98@gmail.com>
2022-10-31 17:06:07 +0000

    work on landing layout, done!

--- resources/js/pages/public/HelloWorld.vue
+++ resources/js/pages/public/HelloWorld.vue
@@ -1,21 +1,41 @@
 <template>
-  <v-container fluid style="height: 100vh;">
-    <v-row justify="center" class="my-4 h-100">
-      <v-col cols="12">
-        <div>
-          <v-img src="img/reading_list_pana.png"></v-img>
-        </div>
-      </v-col>
-      <v-col cols="12" class="font-weight-bold" style="font-family: 'Caveat', cursive;">
-        <p class="text-center" style="font-size: 32px;">Bienvenu sur Nate Bi.</p>
-        <p class="text-center" style="font-size: 18px;">Plateforme de transparence et gestion de cotisation pour
-          l'amélioration de
-          la vie social
-          et
-          communautaire </p>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-container fluid style="height: 100vh">
+        <v-row justify="center" class="h-100">
+            <v-col cols="12">
+                <div>
+                    <v-img src="img/reading_list_pana.png"></v-img>
+                </div>
+            </v-col>
+            <v-col
+                cols="12"
+                class="font-weight-bold"
+                style="font-family: 'Caveat', cursive !important"
+            >
+                <p
+                    class="text-center text-h2 text-md-h1 font-weight-bold title-font"
+                >
+                    Bienvenu sur Nate Bi.
+                </p>
+                <p class="text-center text-h4 text-md-h3 title-font">
+                    Plateforme de transparence et gestion de cotisation pour
+                    l'amélioration de la vie social et communautaire
+                    <!-- Plateforme de transparence et gestion de cotisation pour
+                    l'amélioration de la vie social et communautaire -->
+                </p>
+            </v-col>
+            <v-col cols="12">
+                <p class="text-center">
+                    <v-btn
+                        rounded
+                        large
+                        color="#FF725E"
+                        style="color: whitesmoke"
+                        >Se connecter</v-btn
+                    >
+                </p>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script lang="js">
@@ -32,3 +52,8 @@ export default Vue.extend({
   }
 });
 </script>
+<style>
+p.text-center.title-font {
+    font-family: "Caveat", cursive !important;
+}
+</style>
